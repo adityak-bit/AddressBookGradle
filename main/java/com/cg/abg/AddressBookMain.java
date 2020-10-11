a82e29c61e3d1eee3a6b84d2f6dea8d6889fde30
adityak-bit <adityajsr1998@gmail.com>
2020-10-11 19:27:40 +0530

    [ADITYA] Added : Ability to ensure there is no Duplicate Entry of the same person

--- main/java/com/cg/abg/AddressBookMain.java
+++ main/java/com/cg/abg/AddressBookMain.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		AddressBookList addressBookList = new AddressBookList();
 		Scanner in = new Scanner(System.in);
 		boolean b = false;
 		System.out.println("Welcome to Addressbook");
-      
+
 		System.out.println("Enter 1 to open existing addressbook");
 		System.out.println("Enter 2 to open new addressbook");
 		System.out.println("Enter 3 to exit");
 		int option = in.nextInt();
-		while(option != 3) {
-			if(option == 1)
+		while (option != 3) {
+			if (option == 1)
 				addressBookList.openAddressBook();
-			else if(option == 2)
+			else if (option == 2)
 				addressBookList.newAddressBook();
-			else if(option == 3)
+			else if (option == 3)
 				break;
 			else
 				System.out.println("Enter Correct option");
@@ -28,5 +28,5 @@ public static void main(String[] args) {
 			option = in.nextInt();
 		}
 		System.out.println("Thank You");
-}
+	}
 }
